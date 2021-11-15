f5e12eff0f2a71db5b8cd3b2175e801a474ec3d3
Gavin Eldridge <gchacka@gmail.com>
2021-11-14 20:17:24 -0800

    to new pc

---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Nav: React.FC = (props: any) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Nav.propTypes = {
+
+}
+
+export default Nav;
+
---
+++ frontend/src/components/Routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Routes: React.FC = (props: any) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Routes.propTypes = {
+
+}
+
+export default Routes;
+
+
